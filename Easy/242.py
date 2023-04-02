5db6208d339e235aa22702c07d4b17e4c344ab47
Dhruv Mehta <113890802+dm9612@users.noreply.github.com>
2023-04-02 17:02:11 +0530

    Update 242.py

--- Easy/242.py
+++ Easy/242.py
@@ -8,7 +8,7 @@ class Solution:
             countS[s[i]] = 1 + countS.get(s[i],0)
             countT[t[i]]= 1 + countT.get(t[i],0)
         for c in countS:
-            if countS!=countT:
+            if countS[c]!=countT.get(c,0):
                 return False
         return True
 
