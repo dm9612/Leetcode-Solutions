f73af2a686b94eb734c239ae435db23876a7cfd5
Dhruv Mehta <113890802+dm9612@users.noreply.github.com>
2023-04-02 16:49:44 +0530

    Create 242.py

---
+++ Easy/242.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i],0)
+            countT[t[i]]= 1 + countT.get(t[i],0)
+        for c in countS:
+            if countS!=countT:
+                return False
+        return True
