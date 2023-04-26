ff08b211779934c317171c20d9334639e038e0f2
Dhruv Mehta <113890802+dm9612@users.noreply.github.com>
2023-04-26 21:42:10 +0530

    Create 125.py

---
+++ Easy/125.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        l, r = 0, len(s)-1
+
+        while l<r:
+            while l<r and not self.palindrome(s[l]):
+                l+=1
+            while r>l and not self.palindrome(s[r]):
+                r-=1
+            if s[l].lower() != s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
+        
+    
+    def palindrome(self, c):
+        return( 
+            (ord("a")<= ord(c) <= ord("z")
+            or ord("A")<= ord(c) <= ord("Z")
+            or ord("0")<= ord(c) <= ord("9"))
+        )
+
+
+        
+  
