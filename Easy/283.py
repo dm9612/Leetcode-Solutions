4691913ff3bf0176e87806398fa5e8f5a26d4c80
Dhruv Mehta <113890802+dm9612@users.noreply.github.com>
2023-04-26 23:54:56 +0530

    Create 283.py

---
+++ Easy/283.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        count = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[count] = nums[i]
+                count += 1
+        
+        for i in range(count, len(nums)):
+            nums[i] = 0
+
+        
