0569531380350faf6173a60bb26e9ce8fc126e3b
Dhruv Mehta <113890802+dm9612@users.noreply.github.com>
2023-04-27 09:32:48 +0530

    Update 26.py

--- Easy/26.py
+++ Easy/26.py
@@ -1,11 +1,8 @@
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None
-        while m > 0 and n > 0:
-            if nums1[m-1] >= nums2[n-1]:
-                nums1[m+n-1] = nums1[m-1]
-                m -= 1
-            else:
-                nums1[m+n-1] = nums2[n-1]
-                n -= 1
-        if n > 0:
-            nums1[:n] = nums2[:n]
+    def removeDuplicates(self, nums: List[int]) -> int:
+        l = 1
+        for r in range(1, len(nums)):
+            if nums[r] != nums[r-1]:
+                nums[l] = nums[r]
+                l+=1
+        return l
