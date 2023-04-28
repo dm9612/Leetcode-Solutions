a1590431a7330582cf0ea4d6657bab4217fefee3
Dhruv Mehta <113890802+dm9612@users.noreply.github.com>
2023-04-28 23:53:04 +0530

    Create 682.py

---
+++ Easy/682.py
@@ -0,0 +1,16 @@
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+
+        stack = []
+
+        for op in ops:
+            if op == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif op == 'D':
+                stack.append(stack[-1]*2)
+            elif op == 'C':
+                stack.pop()
+            else:
+                stack.append(int(op))
+        return sum(stack)
+
